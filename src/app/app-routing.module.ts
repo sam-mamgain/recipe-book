84086f7c3cdd8dfa318b7ef4068e62b96a30fd26
sudhanshu <mesudhanshu41@gmail.com>
2021-09-29 17:06:32 +0530

    section 22: Angular modules & optimizing angular apps - Getting started with features modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,12 +12,14 @@ import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
-    { path: '', component: RecipeStartComponent },
-    { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
-    { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] },
-  ] },
+  {
+    path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
+      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] },
+    ]
+  },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent },
 ];
